cda464b2d11affafb13ed821adfe4900b8fe8e87
ApriyantoWibowo <pri.yant0@ovi.com>
2017-06-17 23:26:06 +0700

    Add files via upload

---
+++ rekayasa_web.sql
@@ -0,0 +1,187 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 17, 2017 at 01:57 PM
+-- Server version: 5.6.11
+-- PHP Version: 5.5.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `rekayasa_web`
+--
+CREATE DATABASE IF NOT EXISTS `rekayasa_web` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `rekayasa_web`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `daftar_planet`
+--
+
+CREATE TABLE IF NOT EXISTS `daftar_planet` (
+  `No` int(11) NOT NULL AUTO_INCREMENT,
+  `Nama` varchar(50) NOT NULL,
+  `Keterangan` varchar(500) NOT NULL,
+  PRIMARY KEY (`No`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `daftar_planet`
+--
+
+INSERT INTO `daftar_planet` (`No`, `Nama`, `Keterangan`) VALUES
+(1, 'Merkurius', 'Merkurius adalah planet terkecil di dalam tata surya dan juga yang terdekat dengan Matahari. Kecerahan planet ini berkisar di antara -2 sampai 5,5 dalam magnitudo tampak namun tidak mudah terlihat karena sudut pandangnya dengan matahari kecil. Merkurius hanya bisa terlihat pada saat subuh atau maghrib.'),
+(2, 'Venus', 'Venus atau Bintang Kejora adalah planet terdekat kedua dari matahari setelah Merkurius. Planet ini memiliki radius 6.052 km dan mengelilingi Matahari dalam waktu 225 hari. Atmosfer Venus mengandung 97% karbondioksida (CO2) dan 3% nitrogen, sehingga hampir tidak mungkin terdapat kehidupan.'),
+(3, 'Bumi', 'Bumi adalah planet ketiga dari delapan planet dalam Tata Surya. Diperkirakan usianya mencapai 4,6 milyar tahun. Bumi mempunyai lapisan udara (atmosfer) dan medan magnet yang disebut (magnetosfer) yang melindung permukaan Bumi dari angin matahari, sinar ultraungu, dan radiasi dari luar angkasa. Lapisan udara ini menyelimuti bumi hingga ketinggian sekitar 700 kilometer. Lapisan udara ini dibagi menjadi Troposfer, Stratosfer, Mesosfer, Termosfer, dan Eksosfer.'),
+(4, 'Mars', 'Mars adalah planet terdekat keempat dari Matahari. Namanya diambil dari dewa perang Romawi, Mars. Planet ini sering dijuluki sebagai “planet merah” karena tampak dari jauh berwarna kemerah-kemerahan. Ini disebabkan oleh keberadaan besi(III) oksida di permukaan planet Mars.'),
+(5, 'Jupiter', 'Yupiter atau Jupiter adalah planet terdekat kelima dari matahari setelah Merkurius, Venus, Bumi, dan dan Mars. Jarak rata-rata antara Jupiter dan Matahari adalah 778,3 juta km. Jupiter adalah planet terbesar dan terberat dengan diameter ekuatornya 14.980 km dan memiliki massa 318 kali massa bumi.'),
+(6, 'Saturnus', 'Saturnus adalah planet keenam dari Matahari dan planet kedua terbesar di tata surya, setelah Jupiter. Saturnus, bersama-sama dengan Jupiter, Uranus dan Neptunus, diklasifikasikan sebagai sebuah planet bergas.'),
+(7, 'Uranus', 'Uranus adalah planet ketujuh dari Matahari dan planet yang terbesar ketiga dan terberat keempat dalam Tata Surya. Meskipun Uranus terlihat dengan mata telanjang seperti lima planet klasik, ia tidak pernah dikenali sebagai planet oleh pengamat dahulu kala karena redupnya dan orbitnya yang lambat.'),
+(8, 'Neptunus', 'Neptunus merupakan planet terjauh (kedelapan) jika ditinjau dari Matahari. Planet ini dinamai dari dewa lautan Romawi. Neptunus merupakan planet terbesar keempat berdasarkan diameter (49.530 km) dan terbesar ketiga berdasarkan massa. Massa Neptunus tercatat 17 kali lebih besar daripada Bumi, dan sedikit lebih besar daripada Uranus.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `galaksi`
+--
+
+CREATE TABLE IF NOT EXISTS `galaksi` (
+  `No` int(10) NOT NULL,
+  `Nama` varchar(100) NOT NULL,
+  `Keterangan` varchar(1000) NOT NULL,
+  PRIMARY KEY (`No`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `galaksi`
+--
+
+INSERT INTO `galaksi` (`No`, `Nama`, `Keterangan`) VALUES
+(1, 'Large Magellanic Cloud Galaxy', 'The Large Magellanic Cloud (LMC) adalah sebuah galaksi tidak teratur di dekatnya, dan merupakan satelit dari Bima Sakti. Ia memiliki massa setara dengan sekitar 10 miliar kali massa Matahari (1010 massa matahari), sehingga secara kasar 1 / 10 besar sebagai Bima Sakti, dan diameter sekitar 14.000 tahun cahaya. The LMC adalah galaksi terbesar ke empat di Grup Lokal, tempat terbesar pertama, kedua dan ketiga yang diambil oleh Andromeda Galaxy (M31), kita sendiri Galaksi Bima Sakti, dan Triangulum Galaxy (M33), masing-masing.'),
+(2, 'Andromeda Galaxy', 'Dengan mata telanjang, galaksi ini nampak seperti kabut tipis kecil di langit utara, tapi jika diamati dengan teropong yang dapat menampakkan bintang bintang redup di tepian galaksi Andromeda, ternyata ukuran Andromeda bisa lebih dari 7 kali diamter sudut bulan. Galaksi ini berisi sekitar 1 triliun bintang, dan bergerak mendekati Bima Sakti dengan kecepatan sekitar 300 km/detik.'),
+(3, 'Black Eye Galaxy', 'Sebuah galaksi spiral di konstelasi Coma Berenices, Messier 64, yang terkenal. “Black Eye” galaksi atau “Putri Tidur galaksi,”memiliki spektakuler band gelap menyerap debu di depan inti galaksi cerdas. Itu terkenal di kalangan astronom amatir karena penampilannya di teleskop kecil.'),
+(4, 'Centaurus A Galaxy', 'Centaurus A (juga dikenal sebagai NGC 5128) adalah galaksi lenticular sekitar 11 juta tahun cahaya di konstelasi Centaurus. Ini adalah salah satu radio galaksi terdekat dengan Bumi, jadi inti galaksi aktif telah banyak dipelajari oleh para astronom profesional. galaksi ini juga merupakan terang kelima di langit, sehingga target yang ideal astronomi amatir,meskipun galaksi hanya terlihat dari garis lintang utara rendah dan belahan bumi selatan.'),
+(5, 'Triangulum Galaxy', 'Triangulum Galaxy adalah galaksi spiral sekitar 3 juta tahun cahaya jarak di konstelasi Triangulum. Triangulum Galaxy adalah anggota ketiga-terbesar dari Kelompok Lokal galaksi, yang meliputi Milky Way Galaxy, Galaxy Andromeda dan sekitar 30 galaksi kecil lainnya. Ini adalah salah satu objek permanen yang paling jauh yang dapat dilihat dengan mata telanjang.'),
+(6, 'Whirlpool Galaxy', 'The Whirlpool Galaxy juga dikenal sebagai Messier 51A, M51a, atau NGC 5194, Pusaran Air Galaxy adalah sebuah grand-design berinteraksi galaksi spiral yang terletak pada jarak sekitar 23 juta tahun cahaya di konstelasi Tongkat Venatici. Ini adalah salah satu spiral galaksi paling terkenal di langit.'),
+(7, 'Sunflower Galaxy', 'Sunflower Galaxy (juga dikenal sebagai Messier 63, M63, atau NGC 5055) adalah sebuah galaksi spiral di konstelasi Canes Venatici terdiri dari cakram pusat dikelilingi oleh banyak segmen lengan spiral pendek. Galaxy Sunflower merupakan bagian dari Grup M51, sekelompok galaksi yang juga mencakup Whirlpool Galaxy (M51).'),
+(8, 'Mayall`s Object Galaxy', 'Mayall`s Object adalah hasil dari dua galaksi bertabrakan terletak 500 juta tahun cahaya dalam konstelasi Ursa Major. Hal ini ditemukan oleh Nicholas U. Mayall dari Observatorium Lick pada tanggal 13 Maret 1940, dengan menggunakan reflektor Crossley. Ketika pertama kali ditemukan, Objek Mayall adalah digambarkan sebagai nebula aneh, berbentuk seperti tanda tanya.'),
+(9, 'Small Magellanic Cloud Galaxy', 'Small Magellanic Cloud (SMC) adalah galaksi kerdil. Ia memiliki diameter sekitar 7.000 tahun cahaya dan berisi beberapa ratus juta bintang. Ini memiliki massa total sekitar 7 miliar kali massa Matahari.'),
+(10, 'Comet Galaxy', 'Galaxy Comet adalah galaksi spiral yang terletak 3,2 miliar tahun cahaya dari Bumi, pada gugus galaksi Abell 2667, ditemukan dengan Teleskop luar angkasa Hubble. galaksi ini memiliki sekitar 500 ribu bintang di dalamnya dan memiliki massa sedikit lebih dari Bima Sakti kita.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
+CREATE TABLE IF NOT EXISTS `groups` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `groups`
+--
+
+INSERT INTO `groups` (`id`, `name`, `description`) VALUES
+(1, 'admin', 'Administrator'),
+(2, 'members', 'General User');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_attempts`
+--
+
+CREATE TABLE IF NOT EXISTS `login_attempts` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `ip_address` varchar(15) NOT NULL,
+  `login` varchar(100) NOT NULL,
+  `time` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `ip_address` varchar(45) NOT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `salt` varchar(255) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `activation_code` varchar(40) DEFAULT NULL,
+  `forgotten_password_code` varchar(40) DEFAULT NULL,
+  `forgotten_password_time` int(11) unsigned DEFAULT NULL,
+  `remember_code` varchar(40) DEFAULT NULL,
+  `created_on` int(11) unsigned NOT NULL,
+  `last_login` int(11) unsigned DEFAULT NULL,
+  `active` tinyint(1) unsigned DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `company` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `salt`, `email`, `activation_code`, `forgotten_password_code`, `forgotten_password_time`, `remember_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `company`, `phone`) VALUES
+(1, '127.0.0.1', 'administrator', '$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36', '', 'admin@admin.com', '', NULL, NULL, NULL, 1268889823, 1268889823, 1, 'Admin', 'istrator', 'ADMIN', '0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_groups`
+--
+
+CREATE TABLE IF NOT EXISTS `users_groups` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `group_id` mediumint(8) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uc_users_groups` (`user_id`,`group_id`),
+  KEY `fk_users_groups_users1_idx` (`user_id`),
+  KEY `fk_users_groups_groups1_idx` (`group_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `users_groups`
+--
+
+INSERT INTO `users_groups` (`id`, `user_id`, `group_id`) VALUES
+(1, 1, 1),
+(2, 1, 2);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `users_groups`
+--
+ALTER TABLE `users_groups`
+  ADD CONSTRAINT `fk_users_groups_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_users_groups_groups1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
